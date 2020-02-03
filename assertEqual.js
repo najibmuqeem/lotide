c29254cc699e31a00e6391b5e71f19cb98a49f62
Najib <najib.muqeem@gmail.com>
2020-02-03 18:19:06 +0000

    used template literals instead of +s

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅✅✅ Assertion passed: " + actual + " === " + expected);
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
   } else {
-    console.log("🛑🛑🛑 Assertion failed: " + actual + " !== " + expected);
+    console.log(`🛑🛑🛑 Assertion failed: ${actual} === ${expected}`);
   }
 };
 
