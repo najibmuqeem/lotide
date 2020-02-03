fb38a906cfc7d5e6a50b0928d0436350c8be262a
Najib <najib.muqeem@gmail.com>
2020-02-03 18:08:54 +0000

    assert

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("abcdefg", "abcdefg");
+assertEqual(24, 42);
