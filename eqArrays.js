3cc2e2a8ffdec027315fae0b884fc52e29ebe27b
Najib <najib.muqeem@gmail.com>
2020-02-04 18:28:17 +0000

    finished eqArrays js file

---
+++ eqArrays.js
@@ -0,0 +1,26 @@
+const eqArrays = function(arr1, arr2) {
+  let isTrue = false;
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] === arr2[i]) {
+        isTrue = true;
+      } else {
+        isTrue = false;
+      }
+    }
+  }
+  return isTrue;
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: ${actual} === ${expected}`);
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
