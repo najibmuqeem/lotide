467eeffb64864e622ae7155151343ec90fc74e49
Najib <najib.muqeem@gmail.com>
2020-02-07 18:08:59 +0000

    implemented recursive function to improve eqArrays functionality

--- eqArrays.js
+++ eqArrays.js
@@ -2,7 +2,9 @@ const eqArrays = function(arr1, arr2) {
   let isEqual = true;
   if (arr1.length === arr2.length) {
     for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] === arr2[i]) {
+      if (Array.isArray(arr1[i])) {
+        isEqual = eqArrays(arr1[i], arr2[i]);
+      } else if (arr1[i] === arr2[i]) {
         isEqual = true;
       } else {
         isEqual = false;
@@ -26,3 +28,17 @@ assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+
+assertEqual(
+  eqArrays(
+    [[2, 3], [4]],
+    [
+      [2, 3],
+      [4, 5]
+    ]
+  ),
+  false
+);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
